64f516bb261b03b30a85f865bc9a6f2a66fd0db7
Chaz <chaz.ferguson@gmail.com>
2016-05-17 12:01:05 -0400

    added a +

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ module.exports = {
     // this is the test key from giphy.
     // TODO: replace with another API, or randomize the results?
     rest.get('http://api.giphy.com/v1/gifs/search?q='+query+'&api_key=dc6zaTOxFJmzC').on('complete', function(data) {
-      cb(null, parsedText '! ' + data.data[0].images.fixed_height.url);
+      cb(null, parsedText + '! ' + data.data[0].images.fixed_height.url);
     });
   }
 }
